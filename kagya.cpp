b834df2c031cf63d5e3c817185d95ebab2e81742
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-07-24 20:05:19 +0000

    changes

--- kagya.cpp
+++ kagya.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <map>
 using namespace std;
-void schoolsDisplay()
+/*void schoolsDisplay()
 {
    ifstream schools;
    schools.open("student.csv", ios::in);
@@ -31,10 +32,40 @@ void schoolsDisplay()
          cout << school[i] << endl;
    }
 }
+}*/
+unordered_map<string, string> courseMap;
+
+string courseSelection() {
+string code;
+do{
+   cout << "Enter code: ";
+   cin >> code;
+
+  if (courseMap.find(code) != courseMap.end()) {
+    return courseMap[code];
+  } else {
+      return "Invalid code";
+  }
+
+}while(courseMap.find(code) == courseMap.end());
 }
+
+
 
 int main()
 {
-   schoolsDisplay();
+   cout<<"Select course by typing the course code\n";
+   courseMap["452"] = "General Arts";
+   courseMap["432"] = "General Science";
+   courseMap["111"] = "Business";
+   courseMap["789"] = "Visual Arts";
+   courseMap["182"] = "Home Economics";
+   courseMap["879"] = "Pre-Technical Skills";
+   for (auto it = courseMap.begin(); it != courseMap.end(); ++it)
+   {
+      cout << it->first << ":\t" << it->second << endl;
+   }
+   cout << courseSelection() << endl;
+   //schoolsDisplay();
    return 0;
 }
--- results.cpp
+++ results.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <cstdlib>
+#include <unordered_map>
 using namespace std;
 
 class Student
@@ -41,8 +42,8 @@ class Results {
 
 int English, Math, Science, Social;
 int French, HomeEcons, Local, Pretech;
-
 int res[8];
+unordered_map<string, int> subjects;
 void core_courses()
  {
         cout << "Enter scores for your 4 core courses" << endl;
@@ -50,22 +51,26 @@ void core_courses()
         cout << "Enter your score for English: "<<endl;
         cin >> English;
         res[0] = English;
+        subjects["English"] = English;
         Results english("English", English);
         cout << "Enter your score for Math: "<<endl;
         cin >> Math;
         res[1] = Math;
+        subjects["Math"] = Math;
         Results math("Math", Math);
         cout << "Enter your score for Science: "<<endl;
         cin >> Science;
         res[2] = Science;
+        subjects["Science"] = Science;
         Results science("Science", Science);
         cout << "Enter your score for Social: "<<endl;
         cin >> Social;
         res[3] = Social; 
+        subjects["Social"] = Social;
         Results social("Social", Social);
  }
 
- void optionalCourse()
+void optionalCourse()
  {
     cout << "You have to choose exactly 4 optional courses" << endl;
   int choiceCount = 0;
@@ -82,22 +87,27 @@ void core_courses()
           cout << "Enter your score for French: " << endl;
           cin >> French;
           res[4] = French;
+          subjects["French"] = French;
           break;
         case 2:
           cout << "Enter your score for Home Economics: " << endl;
           cin >> HomeEcons;
           res[5] = HomeEcons;
+          subjects["Home Economics"] = HomeEcons;
           break;
         case 3:
           cout << "Enter your score for Local Language: " << endl;
           cin >> Local;
           res[6] = Local;
+          subjects["Local Language"] = Local;
           break;
         case 4:
           cout << "Enter your score for Pretech: " << endl;
           cin >> Pretech;
           res[7] = Pretech;
+          subjects["Pretech"] = Pretech;
           break;
+        
       }
       choiceCount++;
     }
@@ -108,7 +118,6 @@ void core_courses()
     Results local("Local Language", Local);
     Results pretech("Pretech", Pretech);
  }
-
 int compare (const void * a, const void * b)
 {
     return (*(int*)b - *(int*)a);
@@ -116,67 +125,74 @@ int compare (const void * a, const void * b)
 int grade = 0;
 int grading (int scores)
 {
-   if (scores >= 75)
-   {
-    grade += 1;
-   }
-   if (scores <= 39)
-   {
-    grade += 9;
-   }
-    switch (scores)
-    {
-        case 74:
-        case 73:
-        case 72:
-        case 71:
-        case 70:
-            grade += 2;
-            break;
-        case 69:
-        case 68:
-        case 67:
-        case 66:
-        case 65:
-            grade += 3;
-            break;
-        case 64:
-        case 63:
-        case 62:
-        case 61:
-        case 60:
-            grade += 4;
-            break;
-        case 59:
-        case 58:
-        case 57:
-        case 56:
-        case 55:
-            grade += 5;
-            break;
-        case 54:
-        case 53:
-        case 52:
-        case 51:
-        case 50:
-            grade += 6;
-            break;
-        case 49:
-        case 48:
-        case 47:
-        case 46:
-        case 45:
-            grade += 7;
-            break;
-        case 44:
-        case 43:
-        case 42:
-        case 41:
-        case 40:
-            grade += 8;
-            break;
-    }
-    return grade;
+  if (scores <= 100)
+  {
+      if (scores >= 75)
+      {
+       grade += 1;
+      }
+      if (scores <= 39)
+      {
+       grade += 9;
+      }
+       switch (scores)
+       {
+           case 74:
+           case 73:
+           case 72:
+           case 71:
+           case 70:
+               grade += 2;
+               break;
+           case 69:
+           case 68:
+           case 67:
+           case 66:
+           case 65:
+               grade += 3;
+               break;
+           case 64:
+           case 63:
+           case 62:
+           case 61:
+           case 60:
+               grade += 4;
+               break;
+           case 59:
+           case 58:
+           case 57:
+           case 56:
+           case 55:
+               grade += 5;
+               break;
+           case 54:
+           case 53:
+           case 52:
+           case 51:
+           case 50:
+               grade += 6;
+               break;
+           case 49:
+           case 48:
+           case 47:
+           case 46:
+           case 45:
+               grade += 7;
+               break;
+           case 44:
+           case 43:
+           case 42:
+           case 41:
+           case 40:
+               grade += 8;
+               break;
+       }
+       return grade;
+}
+  else{
+    cout<<"Enter the correct score: "<<endl;
+
+  }
 }
 
 void calc()
@@ -201,10 +217,16 @@ void calc()
 
     cout << "Total grade: " << grade << endl;
 }
+
 int main()
 {
+  
   core_courses();
   optionalCourse();
   calc();
+  cout<<"Your results are as follows\n";
+  for (auto it = subjects.begin(); it != subjects.end(); it++) {
+    std::cout << it->first << " : " << it->second << std::endl;
+  }
   return 0;
-}
+  }
